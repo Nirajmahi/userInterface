6d263694dea3935282444472bf2d90a027ecdb8f
Niraj Mahi <mahiummeniraj1@gmail.com>
2020-10-13 00:03:50 +0600

    user interface

---
+++ d.dart
@@ -0,0 +1,12 @@
+class mahi extends StatefulWidget {
+  @override
+  _mahiState createState() => _mahiState();
+}
+
+class _mahiState extends State<mahi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
